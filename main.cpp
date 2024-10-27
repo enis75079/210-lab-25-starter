3b7b752c5b3a8c9eca337d965ef8bf6b931b570f
enis75079 <naveenislam55@gmail.com>
2024-10-27 15:24:10 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -26,15 +26,11 @@ int main() {
     set<string> tests;
 
     // reading
-
-    for (int i = 0; i < 3; i++) {
-        auto start = high_resolution_clock::now();
-        reading(testv, testl, tests);
-        auto end = high_resolution_clock::now();
-        auto duration = duration_cast<milliseconds>(end - start);
-        cout << duration.count() << endl;
-    }
-
+    auto start = high_resolution_clock::now();
+    readingVec(testv);
+    auto end = high_resolution_clock::now();
+    auto durationV = duration_cast<milliseconds>(end - start);
+    cout << durationV.count() << endl;
 
     return 0;
 }
@@ -48,20 +44,20 @@ void readingVec(vector<string>& v) {
     file.close();
 }
 
-void readingList(list<string>& v) {
+void readingList(list<string>& l) {
     string read;
     ifstream file("codes.txt");
     while(getline(file, read)) {
-        v.push_back(read);
+        l.push_back(read);
     }
     file.close();
 }
 
-void readingSet(set<string>& v) {
+void readingSet(set<string>& s) {
     string read;
     ifstream file("codes.txt");
     while(getline(file, read)) {
-        v.push_back(read);
+        s.insert(read);
     }
     file.close();
 }
